0ad4196afd492364c9098c8d08f776cf0a59d4e9
Shingugitvr000 <Shingugitvr000@gmail.com>
2026-03-26 18:09:07 +0900

    [Feat] 총알 충돌 체크 (Collider 버전)

---
+++ Assets/Scripts/SimpleBullet.cs
@@ -0,0 +1,59 @@
+using Fusion;
+using UnityEngine;
+
+public class SimpleBullet : NetworkBehaviour
+{
+    [SerializeField] private float speed = 10f;
+    [SerializeField] private float lifeTime = 2f;
+    [SerializeField] private float hitRadius = 0.3f;
+
+    [Networked] private TickTimer LifeTimer { get; set; }                   //네트워크의 타이머
+
+    [Networked] private PlayerRef Owner {  get; set; }
+
+    public void Init(PlayerRef owner)
+    {
+        Owner = owner;
+    }
+
+    public override void Spawned()                                              //네트워크상 스폰이 되었을때
+    {
+        if (Object.HasStateAuthority)                                           //오브젝트의 권한이 있을때
+        {
+            LifeTimer = TickTimer.CreateFromSeconds(Runner, lifeTime);          //타이머를 셋팅 한다. 
+        }
+    }
+
+    public override void FixedUpdateNetwork()                                   //네트워크 업데이트에서
+    {
+        if (!Object.HasStateAuthority)
+            return;
+
+        transform.position += transform.forward * speed * Runner.DeltaTime;     //총알은 앞으로 간다.
+
+        if(Object.HasStateAuthority && LifeTimer.Expired(Runner))               //타이머가 만료 될 경우
+        {
+            Runner.Despawn(Object);                                             //디스폰한다.
+        }
+
+        Collider[] hits = Physics.OverlapSphere(transform.position, hitRadius);
+
+        foreach(var hit in hits)
+        {
+            SimplePlayer player = hit.GetComponentInParent<SimplePlayer>();
+
+            if (player == null)
+                continue;
+
+            if (player.Object.InputAuthority == Owner)
+                continue;
+
+            Debug.Log($"총알이 플레이어를 맞춤 : {player.Object.InputAuthority}");
+
+            Runner.Despawn(Object);
+
+            return;
+        }
+    }
+
+}
